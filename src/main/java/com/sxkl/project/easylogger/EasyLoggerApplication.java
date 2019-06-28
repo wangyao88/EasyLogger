427d713dbbeef7b16243b9b4605b8679960ea121
wangyao <wangyao@cis.com.cn>
2019-06-28 16:54:36 +0800

    项目初始化

---
+++ src/main/java/com/sxkl/project/easylogger/EasyLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.sxkl.project.easylogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyLoggerApplication.class, args);
+	}
+
+}
